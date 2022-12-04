17b6b7bfe1ee44fe418399120d9ef225b8709351
Natig Kurbanov <natig_kurbanov@epam.com>
2022-12-05 00:27:26 +0400

    fixed an issue with secret

--- src/main/java/dev/natig/finalprojectgcp/controller/PurchaseController.java
+++ src/main/java/dev/natig/finalprojectgcp/controller/PurchaseController.java
@@ -1,16 +1,21 @@
 package dev.natig.finalprojectgcp.controller;
 
 import dev.natig.finalprojectgcp.mapper.PurchaseMapper;
-import dev.natig.finalprojectgcp.model.domain.Purchase;
 import dev.natig.finalprojectgcp.model.dto.PurchaseDto;
 import dev.natig.finalprojectgcp.repository.PurchaseRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
